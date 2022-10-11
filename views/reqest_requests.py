2fc1079899ab48f59db93f113ed8b6914ec58d79
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-10-11 11:40:36 -0500

    Created Request List in request-request fle

---
+++ views/reqest_requests.py
@@ -0,0 +1,29 @@
+REQUESTS = [
+    {
+        "id": 1,
+        "parentName": "Brandon",
+        "childName": "Nora",
+        "numberOfChildren": 2,
+        "address": "1234 Street",
+        "dateOfReservation": 20221011,
+        "lengthOfReservation": 5
+    },
+    {
+        "id": 2,
+        "parentName": "Nate",
+        "childName": "Jermey",
+        "numberOfChildren": 2,
+        "address": "45678 Street",
+        "dateOfReservation": 20221012,
+        "lengthOfReservation": 4
+    },
+    {
+        "id": 3,
+        "parentName": "Shane",
+        "childName": "Steve",
+        "numberOfChildren": 2,
+        "address": "1234 Street",
+        "dateOfReservation": 20221013,
+        "lengthOfReservation": 2
+    }
+]
