5f8b675e2a517b691f2eab7ded64a4eb48695b0b
Jeremy Myers <echelonical@gmail.com>
2022-10-11 11:50:57 -0500

    created delete methode

--- views/reqest_requests.py
+++ views/reqest_requests.py
@@ -27,3 +27,19 @@ REQUESTS = [
         "lengthOfReservation": 2
     }
 ]
+
+
+def delete_request(id):
+    # Initial -1 value for request index, in case one isn't found
+    request_index = -1
+
+    # Iterate the requestS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, request in enumerate(REQUESTS):
+        if request["id"] == id:
+            # Found the request. Store the current index.
+            request_index = index
+
+    # If the request was found, use pop(int) to remove it from list
+    if request_index >= 0:
+        REQUESTS.pop(request_index)
