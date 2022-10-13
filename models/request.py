71f5de712f0b6bfe2f66c73fea82b36b69c2b710
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-10-13 11:43:15 -0500

    Added models package

---
+++ models/request.py
@@ -0,0 +1,12 @@
+from tomlkit import date
+
+
+class Request():
+    def __init__(self, id, parentName, childName, address, numberOfChildren, dateOfReservation, lengthOfReservation):
+        self.id = id
+        self.parentName = parentName
+        self.childName = childName
+        self.address = address
+        self.numberOfChildren = numberOfChildren
+        self.dateOfReservation = dateOfReservation
+        self.lengthOfReservation = lengthOfReservation
